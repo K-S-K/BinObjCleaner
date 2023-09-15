3688da8f31af44b28329fa20555419ec5e9369e8
Stanislav Kiselevskii <kisilevski@gmail.com>
2023-09-15 11:55:35 +0400

    Add project files.

---
+++ BinObjCleaner/DirectoryTree.cs
@@ -0,0 +1,79 @@
+﻿using System.Data;
+
+namespace BinObjCleaner
+{
+    public class DirectoryTree
+    {
+        public readonly string Name;
+        public readonly string Path;
+
+        public readonly bool ContainsObjOrBin;
+        public readonly bool HasObjOrBinDirectly;
+
+        public bool IsObjOrBin =>
+            Name.Equals("bin", comparisonType: StringComparison.OrdinalIgnoreCase) ||
+            Name.Equals("obj", comparisonType: StringComparison.OrdinalIgnoreCase) ||
+            Name.Equals("_obj", comparisonType: StringComparison.OrdinalIgnoreCase);
+
+        public bool ToSkip => IsObjOrBin ||
+            Name.Equals(".vs", comparisonType: StringComparison.OrdinalIgnoreCase) ||
+            Name.Equals(".git", comparisonType: StringComparison.OrdinalIgnoreCase) ||
+            Name.Equals(".nuget", comparisonType: StringComparison.OrdinalIgnoreCase) ||
+            Name.Equals("packages", comparisonType: StringComparison.OrdinalIgnoreCase);
+
+        public bool NeedNoAction =>
+            (!IsObjOrBin && ToSkip)
+            && !HasObjOrBinDirectly
+            && !ContainsObjOrBin
+            && !IsObjOrBin;
+
+        public List<DirectoryTree> Branches { get; private set; }
+
+        public override string ToString() => $"{Name}, [{Branches.Count}]{(IsObjOrBin ? " DEL!" : HasObjOrBinDirectly ? " Has!" : "")}";
+
+        public void Grow()
+        {
+            if (ToSkip)
+            {
+                return;
+            }
+
+            var subdirs = Directory.GetDirectories(Path).OrderBy(x => x);
+            foreach (var dir in subdirs)
+            {
+                DirectoryTree tree = new DirectoryTree(dir);
+                tree.Grow();
+                Branches.Add(tree);
+            }
+        }
+
+        public IEnumerable<string> DirsToDel
+        {
+            get
+            {
+                List<string> dirs = new();
+                dirs.AddRange(Branches.Where(x => x.IsObjOrBin).Select(x => x.Path));
+
+                foreach (var tree in Branches)
+                {
+                    dirs.AddRange(tree.DirsToDel);
+                }
+
+                return dirs.Distinct();
+            }
+        }
+
+        public DirectoryTree(string path)
+        {
+            Branches = new List<DirectoryTree>();
+            Path = path ?? throw new ArgumentNullException(path);
+
+            Name = Path.Substring(Path.LastIndexOf('\\')).TrimStart('\\');
+            Grow();
+
+            HasObjOrBinDirectly = Branches.Where(x => x.IsObjOrBin).Any();
+
+            ContainsObjOrBin = Branches.Where(x => x.IsObjOrBin || x.ContainsObjOrBin).Any();
+        }
+    }
+}
---
+++ BinObjCleaner/Program.cs
@@ -0,0 +1,118 @@
+﻿namespace BinObjCleaner
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string initialDir = @" ";
+            if (args.Length > 0)
+            {
+                initialDir = args[0];
+            }
+
+            while (true)
+            {
+                if (!AskForDirectory(ref initialDir))
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("Exit");
+                    return;
+                }
+
+                ClearAndReport(initialDir);
+
+                Console.WriteLine();
+                Console.WriteLine("Clearing complete. Press Enter to repeat or Esc for exit:");
+
+                ConsoleKeyInfo key = Console.ReadKey();
+                if (key.Key == ConsoleKey.Escape)
+                {
+                    return;
+                }
+                Console.Clear();
+            }
+        }
+
+        private static bool AskForDirectory(ref string initialDir)
+        {
+            while (!Path.Exists(initialDir))
+            {
+                Console.WriteLine("Enter the path or empty string for exit:");
+                initialDir = Console.ReadLine();
+
+                if (string.IsNullOrWhiteSpace(initialDir))
+                {
+                    return false;
+                }
+            }
+
+            return true; 
+        }
+
+        private static void ClearAndReport(string initialDir)
+        {
+            DirectoryTree tree = new(initialDir);
+
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine($"Clear Binary and intermediate files from {tree.Path}");
+            Console.WriteLine();
+
+            WriteContent(tree);
+
+            Console.WriteLine();
+            Console.WriteLine();
+
+            var toDel = tree.DirsToDel;
+
+            if (toDel.Any())
+            {
+                Console.WriteLine($"Clearing list[{toDel.Count()}]:");
+
+                foreach (string dir in toDel)
+                {
+                    string report;
+                    try
+                    {
+                        Directory.Delete(dir, true);
+                        report = "Removed";
+                        Console.ForegroundColor = ConsoleColor.Green;
+                    }
+                    catch (Exception ex)
+                    {
+                        report = $"Failed: {ex.Message}";
+                        Console.ForegroundColor = ConsoleColor.Red;
+                    }
+
+                    Console.WriteLine($"{report}: {dir}");
+                }
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Complete");
+        }
+
+        private static void WriteContent(DirectoryTree tree)
+        {
+            foreach (var br in tree.Branches)
+            {
+                if (br.IsObjOrBin)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                }
+                else if (br.HasObjOrBinDirectly)
+                {
+                    Console.ForegroundColor = ConsoleColor.DarkYellow;
+                }
+                else if (br.ContainsObjOrBin)
+                {
+                    Console.ForegroundColor = ConsoleColor.DarkRed;
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                }
+                Console.WriteLine(br);
+            }
+        }
+    }
+}
